f6ac5329aa9fbbc49854bf13723b9af89ba0651e
BenjaminDurante <benjamin.durante@ucalgary.ca>
2021-06-05 12:13:02 -0600

    Correct doublet.lua
    Re-arranged sections to correct the lag bug that was experienced. (When switching doublet source, two doublets were required before the correct one was executed).

--- doublet.lua
+++ doublet.lua
@@ -3,7 +3,7 @@
 
 local DOUBLET_ACTION_CHANNEL = 6 -- RCIN channel to start a doublet when high (>1700)
 local DOUBLET_CHOICE_CHANNEL = 7 -- RCIN channel to choose elevator (low) or rudder (high)
-local DOUBLET_FUCNTION = 19 -- which control surface (SERVOx_FUNCTION) number will have a doublet happen
+local DOUBLET_FUNCTION = 19 -- which control surface (SERVOx_FUNCTION) number will have a doublet happen
 -- A (Servo 1, Function 4), E (Servo 2, Function 19), and R (Servo 4, Function 21)
 local DOUBLET_MAGNITUDE = 6 -- defined out of 45 deg used for set_output_scaled
 local DOUBLET_TIME = 500 -- period of doublet signal in ms
@@ -26,7 +26,7 @@ local end_time = -1
 local now = -1
 
 -- store information about the vehicle
-local doublet_srv_chan = SRV_Channels:find_channel(DOUBLET_FUCNTION)
+local doublet_srv_chan = SRV_Channels:find_channel(DOUBLET_FUNCTION)
 local doublet_srv_min = param:get("SERVO" .. doublet_srv_chan + 1 .. "_MIN")
 local doublet_srv_max = param:get("SERVO" .. doublet_srv_chan + 1 .. "_MAX")
 local doublet_srv_trim = param:get("SERVO" .. doublet_srv_chan + 1 .. "_TRIM")
@@ -53,32 +53,35 @@ function doublet()
             -- this is the time that we started
             -- stop time will be start_time + DOUBLET_TIME
             start_time = now
-            -- notify the gcs that we are starting a doublet
-            gcs:send_text(6, "STARTING DOUBLET " .. DOUBLET_FUCNTION)
-            -- get info about the doublet channel
-            doublet_srv_chan = SRV_Channels:find_channel(DOUBLET_FUCNTION)
-            doublet_srv_min = param:get("SERVO" .. doublet_srv_chan + 1 .. "_MIN")
-            doublet_srv_max = param:get("SERVO" .. doublet_srv_chan + 1 .. "_MAX")
-            doublet_srv_trim = param:get("SERVO" .. doublet_srv_chan + 1 .. "_TRIM")
-            pre_doublet_mode = vehicle:get_mode()
+            
             -- are we doing a doublet on elevator or rudder? set the other controls to trim
             local doublet_choice_pwm = rc:get_pwm(DOUBLET_CHOICE_CHANNEL)
             local trim_funcs = {}
             local pre_doublet_elevator = SRV_Channels:get_output_pwm(K_ELEVATOR)
             if doublet_choice_pwm < 1500 then
                 -- doublet on elevator
-                DOUBLET_FUCNTION = K_ELEVATOR
+                DOUBLET_FUNCTION = K_ELEVATOR
                 trim_funcs = {K_AILERON, K_RUDDER}
                 DOUBLET_MAGNITUDE = 12
                 doublet_srv_trim = pre_doublet_elevator
             else
                 -- doublet on rudder
-                DOUBLET_FUCNTION = K_RUDDER
+                DOUBLET_FUNCTION = K_RUDDER
                 trim_funcs = {K_AILERON}
                 DOUBLET_MAGNITUDE = 15
                 -- pin elevator to current position. This is most likely different than the _TRIM value
                 SRV_Channels:set_output_pwm_chan_timeout(SRV_Channels:find_channel(K_ELEVATOR), pre_doublet_elevator, DOUBLET_TIME * 4)
             end
+            -- notify the gcs that we are starting a doublet
+            gcs:send_text(6, "STARTING DOUBLET " .. DOUBLET_FUNCTION)
+
+            -- get info about the doublet channel
+            doublet_srv_chan = SRV_Channels:find_channel(DOUBLET_FUNCTION)
+            doublet_srv_min = param:get("SERVO" .. doublet_srv_chan + 1 .. "_MIN")
+            doublet_srv_max = param:get("SERVO" .. doublet_srv_chan + 1 .. "_MAX")
+            doublet_srv_trim = param:get("SERVO" .. doublet_srv_chan + 1 .. "_TRIM")
+            pre_doublet_mode = vehicle:get_mode()
+
             -- set the channels that need to be still to trim until the doublet is done
             for i = 1, #trim_funcs do
                 local trim_chan = SRV_Channels:find_channel(trim_funcs[i])
@@ -95,6 +98,7 @@ function doublet()
             -- enter manual mode
             retry_set_mode(MODE_MANUAL)
         end
+        
         -- split time evenly between high and low signal
         if now < start_time + (DOUBLET_TIME / 2) then
             down = doublet_srv_trim - math.floor((doublet_srv_trim - doublet_srv_min) * (DOUBLET_MAGNITUDE / 45))
@@ -112,11 +116,13 @@ function doublet()
         else
             gcs:send_text(6, "this should not be reached")
         end
-    elseif end_time ~= -1 and rc:get_pwm(DOUBLET_ACTION_CHANNEL) > 1700 then
+    
+    elseif end_time ~= -1 and rc:get_pwm(DOUBLET_ACTION_CHANNEL) > 1700 then -- observation time
         -- wait for RC input channel to go low
         gcs:send_text(6, "RC" .. DOUBLET_ACTION_CHANNEL .. " still high")
         callback_time = 1000 -- prevents spamming messages to the GCS
-    elseif now ~= -1 and end_time ~= -1 then
+    
+    elseif now ~= -1 and end_time ~= -1 then -- normal exit
         gcs:send_text(6, "RETURN TO PREVIOUS FLIGHT MODE")
         now = -1
         end_time = -1
@@ -129,7 +135,8 @@ function doublet()
         end
         retry_set_mode(pre_doublet_mode)
         callback_time = 100 -- don't need to rerun for a little while
-    elseif now ~= -1 then
+    
+    elseif now ~= -1 then -- emergency recovery
         -- stopped before finishing. recover to level attitude
         gcs:send_text(6, "FBWA RECOVER")
         now = -1
@@ -147,5 +154,5 @@ function doublet()
     return doublet, callback_time
 end
 
-gcs:send_text(6, "doubletv3.lua is running")
+gcs:send_text(6, "doublet.lua is running")
 return doublet(), 1000
